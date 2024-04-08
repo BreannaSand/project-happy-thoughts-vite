3a346dc76a6cefcfaec31549d130b8a7343400c4
Breanna Sand <breanna.sand8@gmail.com>
2024-04-08 09:54:17 +0200

    form

--- src/components/ThoughtsForm.jsx
+++ src/components/ThoughtsForm.jsx
@@ -4,7 +4,15 @@ export const ThoughtsForm = () => {
     return (
         <div className="post-wrapper">
             <h2>What is making you happy right now?</h2>
-
+            <form>
+                <textarea rows="3" placeholder="'If music be the food of love, play on.' - William Shakespeare"></textarea>
+                <div className="post-length">
+                    <p className="length">
+                        "0"
+                        "/140"
+                    </p>
+                </div>
+            </form>
         </div>
     )
 }
